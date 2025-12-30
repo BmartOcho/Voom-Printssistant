1f9d1cc531d6466483d8cea297a31fda792ff33e
BmartOcho <109639555+BmartOcho@users.noreply.github.com>
2025-12-30 11:28:23 -0600

    Fixed Canva App, reset to "hello world" state for rebuild

--- canva-app/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import "@canva/app-ui-kit/styles.css";
-import "./styles/app.css";
-import { prepareDesignEditor } from "@canva/intents/design";
-import designEditorIntent from "./intents/design_editor";
-
-// Register the Design Editor intent with Canva
-prepareDesignEditor(designEditorIntent);
